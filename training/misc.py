44f68c6fe81cc38e10e07c1e424e8ed5512ee965
Atidhaya <secrettears53410@gmail.com>
2019-07-05 11:18:50 +0700

    add resuming function

--- training/misc.py
+++ training/misc.py
@@ -125,7 +125,7 @@ def locate_latest_pkl():
     resume_run_id = os.path.basename(os.path.dirname(latest_pickle))
     RE_KIMG = re.compile('network-snapshot-(\d+).pkl')
     kimg = int(RE_KIMG.match(os.path.basename(latest_pickle)).group(1))
-+   return (locate_network_pkl(resume_run_id), float(kimg))
+    return (locate_network_pkl(resume_run_id), float(kimg))
 
 def locate_network_pkl(run_id_or_run_dir_or_network_pkl, snapshot_or_network_pkl=None):
     for candidate in [snapshot_or_network_pkl, run_id_or_run_dir_or_network_pkl]:
